85ddc612b9edf692b2bad7183c035e549213cfc9
jazk <jazk@viessmann.com>
2018-03-22 16:12:41 +0100

    REST API for blog posts

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from .views import post_list_api, PostData, PostList
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', PostList.as_view(), name='home'),
+    url(r'^api/posts/$', post_list_api, name='api-posts'),
+    url(r'^api/post/data/$', PostData.as_view(), name='api-post-data'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.http import JsonResponse
 from django.utils import timezone
+from django.views.generic import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 def post_detail(request, pk):
@@ -8,6 +12,28 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, '../templates/post_list.html', {'posts': posts})
+def post_list_api(request, *args, **kwargs):
+    data = {
+        "posts": Post.objects.count()
+    }
+    return JsonResponse(data)
+
+
+class PostList(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, '../templates/post_list.html')
+
+
+class PostData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+        data = {
+            "posts": [{"title": post.title,
+                       "body": post.text,
+                       "published_date": post.published_date} for i, post in enumerate(posts)]
+        }
+        return Response(data)
